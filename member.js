e4a4e41364837d616cece08ada88c549eb64c7cb
Sebastian Lechte <sebastian.lechte@web.de>
2024-11-22 14:49:31 +0000

    Copilot second Commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    console.log("Your skills: ");
+    console.log("1. NodeJS");
+    console.log("2. ReactJS");
+    console.log("3. React Native");
+    console.log("4. TypeScript");
+    console.log("5. HTML");
+    console.log("6. CSS");
+}
